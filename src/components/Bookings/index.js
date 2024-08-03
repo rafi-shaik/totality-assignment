76d35e34d5b44690c6fe8705b0557492ee67c5a0
rafi-shaik <rafishaik8208@gmail.com>
2024-08-03 18:30:27 +0530

    minor

--- src/components/Bookings/index.js
+++ src/components/Bookings/index.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import { useBookings } from '../../Context/BookingContext';
-import { useCart } from '../../Context/CartContext';
-import './index.css';
+import React from "react";
+import { useBookings } from "../../Context/BookingContext";
+import { useCart } from "../../Context/CartContext";
+import "./index.css";
 
 const Bookings = () => {
   const { bookings } = useBookings();
-  const { addToCart } = useCart();
+  const { addToCart, cart } = useCart();
 
   return (
-    <div className='bookings'>
-      <h1 className='booking-head'>My Bookings</h1>
+    <div className="bookings">
+      <h1 className="booking-head">My Bookings</h1>
       {bookings && bookings.length > 0 ? (
-        bookings.map((property) => (
-          <div key={property.id} className='property-card'>
-            <div className='img-container'>  <img  className='book-img' src={property.image} alt={property.title} /> </div>
-          
-          <div className='des-cont'> 
-            <h2 className='title'>{property.title}</h2>
-            <p className='loca'>{property.location}</p>
-            <p className='loca'>{property.description}</p>
-            <p className='loca'>Price:{property.price}\-</p>
-            <p className='loca'>Bedrooms:{property.bedrooms}</p>
-            <p className='loca'>Availability:{property.availability}</p>
-            <p className='loca'>Tenant type:{property.tenant_type}</p>
-           
-            <button className='add-to-cart' onClick={() => addToCart(property)}>Add to Cart</button></div>
-            
-          </div>
-        ))
+        bookings.map((property) => {
+          const isAdded = cart.some((item) => item.id === property.id);
+
+          return (
+            <div key={property.id} className="property-card">
+              <div className="img-container">
+                {" "}
+                <img
+                  className="book-img"
+                  src={property.image}
+                  alt={property.title}
+                />{" "}
+              </div>
+
+              <div className="des-cont">
+                <h2 className="title">{property.title}</h2>
+                <p className="loca">{property.location}</p>
+                <p className="loca">{property.description}</p>
+                <p className="loca">Price:{property.price}\-</p>
+                <p className="loca">Bedrooms:{property.bedrooms}</p>
+                <p className="loca">Availability:{property.availability}</p>
+                <p className="loca">Tenant type:{property.tenant_type}</p>
+
+                <button
+                  className="add-to-cart"
+                  onClick={() => addToCart(property)}
+                  disabled={isAdded}
+                >
+                  {isAdded
+                    ? " Added to Cart. Please check the cart."
+                    : "Add to Cart"}
+                </button>
+              </div>
+            </div>
+          );
+        })
       ) : (
         <p>No bookings yet.</p>
       )}
--- src/components/FilteredProperties/index.js
+++ src/components/FilteredProperties/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { useBookings } from '../../Context/BookingContext';
-import './index.css';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { useBookings } from "../../Context/BookingContext";
+import "./index.css";
 
 const FilteredProperties = () => {
   const location = useLocation();
@@ -11,26 +11,35 @@ const FilteredProperties = () => {
 
   return (
     <div className="filtered-properties-container">
-      <h1  className='booking-head'>Filtered Properties</h1>
+      <h1 className="booking-head">Filtered Properties</h1>
       {filteredProperties.length > 0 ? (
         <div className="property-list">
           {filteredProperties.map((property) => {
-            const isBooked = bookings.some((booking) => booking.id === property.id);
+            const isBooked = bookings.some(
+              (booking) => booking.id === property.id
+            );
 
             return (
               <div key={property.id} className="property-card">
-                <img src={property.image} alt={property.title} className='book-img' />
+                <img
+                  src={property.image}
+                  alt={property.title}
+                  className="book-img"
+                />
                 <div className="property-details">
                   <h2>{property.title}</h2>
                   <p>{property.description}</p>
                   <p>Price: {property.price}</p>
                   <p>Location: {property.location}</p>
                   <p>Bedrooms: {property.bedrooms}</p>
-                  <p>Amenities: {property.amenities.join(', ')}</p>
+                  <p>Amenities: {property.amenities.join(", ")}</p>
                   <p>Availability: {property.availability}</p>
                   <p>Tenant Type: {property.tenant_type}</p>
-                  <button onClick={() => addBooking(property)} disabled={isBooked}>
-                    {isBooked ? 'Booked' : 'Book'}
+                  <button
+                    onClick={() => addBooking(property)}
+                    disabled={isBooked}
+                  >
+                    {isBooked ? "Booked" : "Book"}
                   </button>
                 </div>
               </div>
